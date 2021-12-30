9c7d5145efa4c8f2a5de9c12dd8fdf9303b288a1
blackbox <dzzirima@gmail.com>
2021-12-30 23:03:20 +0200

    done with deploment to real blockchain

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,5 +18,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.0",
-  net
+  networks:{
+    rinkeby:{
+      url:"https://eth-rinkeby.alchemyapi.io/v2/Cl7f_jSIeYlbHEiC6qoJqAHoXBi9ppq_",
+      accounts:["fac0a0cc2cfdfd77952a011e1d9786d6b229ac40ea00cd23376f572e971c48df"]
+    }
+  }
 };
