e328ec8ee9cd5aa128fe46208db34e569c24a7f4
blackbox <dzzirima@gmail.com>
2021-12-31 22:13:24 +0200

    sending of ether to smart contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,18 +5,55 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
+    event NewWave(address indexed from, uint256 timestamp,string message);
+
+    struct Wave{
+        address waver;
+        string message;
+        uint256 timestamp;
+
+    }
+     /*
+     * I declare a variable waves that lets me store an array of structs.
+     * This is what lets me hold all the waves anyone ever sends to me!
+     */
+    Wave[] waves;
+
+
+
+
     mapping(address => uint256) wavePerWaver;
 
     constructor() {
         console.log("Building something on web3 tech , kudos buildspace");
     }
 
-    function wave() public {
+    function wave(string memory _message) public {
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
 
         // increase the number of waves by the user
         wavePerWaver[msg.sender] += 1;
+
+        // storing wave data
+        waves.push(Wave(msg.sender,_message,block.timestamp));
+
+
+        // emiting an event when we fire an event
+        emit NewWave(msg.sender, block.timestamp, _message);
+
+        //sending some ether to people
+        uint256 prizeAmount = 0.0001 ether;
+
+        require(
+            prizeAmount <= address(this).balance,
+            "Trying to withdrwa more money than the contract  has."
+        );
+
+        (bool success,) = (msg.sender).call{value:prizeAmount}("");
+        require(success, "Failed to withdraw money from contract");
+
+
     }
 
     function getTotalWaves() public view returns (uint256) {
@@ -28,5 +65,10 @@ contract WavePortal {
     function getWavesPerWaver() public view returns (uint256) {
         console.log("%s has has waved  %d times ",msg.sender, wavePerWaver[msg.sender]);
         return 1;
+    }
+
+    // getting all the waves 
+    function getAllWaves() public view returns (Wave[] memory){
+        return waves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,34 +1,54 @@
 const main = async () =>{
     const [owner,randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
+    
+    const waveContract = await waveContractFactory.deploy(({
+        value:hre.ethers.utils.parseEther("0.1"),
+    }));
     await waveContract.deployed();
 
     console.log("Contract deployed to :",waveContract.address)
     console.log("Contract deployed by:",owner.address)
 
+
+// get contact balance
+let contractBalance = await hre.ethers.provider.getBalance(waveContract.address)
+console.log("Contract Balance :",hre.ethers.utils.format(contractBalance))
+
+
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
 
-    let waveTxn = await waveContract.wave()
+    let waveTxn = await waveContract.wave("Walla first wave")
     await waveTxn.wait();
 
-    let waveTxn1 = await waveContract.wave()
-    await waveTxn1.wait();
+    // getting contract balance and see what  happened
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log(
+      "Contract balance:",
+      hre.ethers.utils.formatEther(contractBalance)
+    );
+  
+
+
 
     
 
-    waveCount = await waveContract.getTotalWaves();
 
     //simulating other people interacting with our smart  contract
-    waveTxn = await waveContract.connect(randomPerson).wave();
-    await waveTxn.wait()
+    // waveTxn = await waveContract.connect(randomPerson).wave("Wala third wave");
+    // await waveTxn.wait()
 
     waveCount = await waveContract.getTotalWaves()
 
     // getting number of waves per user
     let wavesPerWaver;
     wavesPerWaver = await waveContract.getWavesPerWaver();
+
+    //getting all waves
+    let allWaves = await waveContract.getAllWaves()
+    console.log(allWaves)
+
     
 
 
